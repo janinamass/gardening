75b382376fe431163354bc6bbdeea5d9d5e93aff
jmass <janina.mass@hhu.de>
2014-05-19 12:36:51 +0200

    prepare for packaging

---
+++ Scythe2/README.txt
@@ -0,0 +1,26 @@
+=======
+Scythe
+=======
+
+scythe, Selection of Conserved Transcript by Homology Evaluation, finds the best matching set of transcripts for each one-to-one orthologous gene from two or more species.
+
+
+Requirements
+============
+
+External programs
+------------------
+needleall (CITE and LINK)
+
+
+Todo
+====
+* document this
+
+
+Contributors
+============
+
+
+Thanks also to
+==============
---
+++ Scythe2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+        name='scythe2',
+        version='0.1',
+        author='Janina Mass',
+        author_email='janina.mass@hhu.de',
+        packages=find_packages(),
+        scripts=['scythe2/convert/scythe_loc_gff.py'],
+        license='GPLv3',
+        description='Find best set of transcripts for one-to-one orthologous genes from two or more species',
+        long_description=open('README.txt').read(),
+        classifiers=[
+            'Topic :: Scientific/Engineering :: Bio-Informatics'
+            ],
+        )
